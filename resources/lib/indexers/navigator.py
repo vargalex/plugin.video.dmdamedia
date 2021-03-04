947581d5828746001b117057b6691e1095b16872
Gabor Varga <gavarga@npsh.hu>
2021-03-04 18:58:00 +0100

    Matrix kompatibilitás

--- resources/lib/indexers/navigator.py
+++ resources/lib/indexers/navigator.py
@@ -19,14 +19,22 @@
 '''
 
 
-import os,sys,re,xbmc,xbmcgui,xbmcplugin,xbmcaddon,urllib,urlparse,base64,time, locale
+import os,sys,re,xbmc,xbmcgui,xbmcplugin,xbmcaddon, time, locale
 import urlresolver
 from resources.lib.modules import client
+from resources.lib.modules.utils import py2_encode
+
+if sys.version_info[0] == 3:
+    import urllib.parse as urlparse
+    from urllib.parse import quote_plus
+else:
+    import urlparse
+    from urllib import quote_plus
 
 sysaddon = sys.argv[0] ; syshandle = int(sys.argv[1])
 addonFanart = xbmcaddon.Addon().getAddonInfo('fanart')
 
-base_url = 'aHR0cHM6Ly9kbWRhbWVkaWEuaHU='.decode('base64')
+base_url = 'https://dmdamedia.hu'
 
 class navigator:
     def __init__(self):
@@ -63,7 +71,7 @@ class navigator:
             items.sort(cmp=locale.strcoll)
             file.close()
             for item in items:
-                self.addDirectoryItem(item, 'items&url=%s&group=%s&search=%s' % (url, group, urllib.quote_plus(item)), '', 'DefaultFolder.png')
+                self.addDirectoryItem(item, 'items&url=%s&group=%s&search=%s' % (url, group, quote_plus(item)), '', 'DefaultFolder.png')
             if len(items) > 0:
                 self.addDirectoryItem('Keresési előzmények törlése', 'deletesearchhistory&url=%s' % url, '', 'DefaultFolder.png') 
         except:
@@ -96,7 +104,7 @@ class navigator:
             else:
                 self.addDirectoryItem('Keresés', 'search&url=%s&group=%s' % (url, group), '', 'DefaultFolder.png')
         url_content = client.request('%s/%s' % (base_url, url))
-        center = client.parseDOM(url_content, 'div', attrs={'class': 'center'})[0].encode('utf-8')
+        center = py2_encode(client.parseDOM(url_content, 'div', attrs={'class': 'center'})[0])
         movies = center.replace('</div>', '</div>\n')
         for line in movies.splitlines():
             matches = re.search(r'^<div class="([^"]*)%s([^"]*)"(.*)data-cim="([^"]*)"(.*)href="([^"]*)"(.*)data-src="([^"]*)(.*)$' % (group if group != "mind" else " "), line.strip())
@@ -107,18 +115,18 @@ class navigator:
                     if search not in lowerLine:
                         isOK = False
                 if isOK:        
-                    self.addDirectoryItem(matches.group(4), '%s&url=%s&thumb=%s' % ("movie" if url != "" else "series", matches.group(6), urllib.quote_plus(matches.group(8))), "%s%s" % (base_url, matches.group(8)), 'DefaultMovies.png' if url != "" else 'DefaultTVShows.png')
+                    self.addDirectoryItem(matches.group(4), '%s&url=%s&thumb=%s' % ("movie" if url != "" else "series", matches.group(6), quote_plus(matches.group(8))), "%s%s" % (base_url, matches.group(8)), 'DefaultMovies.png' if url != "" else 'DefaultTVShows.png')
         self.endDirectory('movies' if url != "" else 'tvshows')
 
     def getSeries(self, url, thumb):
         url_content = client.request('%s%s' %(base_url, url))
         title = client.parseDOM(url_content, 'div', attrs={'class': 'cim'})[0]
-        title = client.replaceHTMLCodes(client.parseDOM(title, 'h1')[0]).encode('utf-8').strip()
+        title = py2_encode(client.replaceHTMLCodes(client.parseDOM(title, 'h1')[0])).strip()
         panel = client.parseDOM(url_content, 'div', attrs={'class': 'panel'})[0]
         poster = client.parseDOM(panel, 'div', attrs={'class': 'poster'})[0]
         banner = '%s/%s' % (poster, client.parseDOM(panel, 'img', ret='src')[0])
         info = client.parseDOM(panel, 'div', attrs={'class': 'info'})[0]
-        plot = client.parseDOM(info, 'div', attrs={'class': 'leiras'})[0].encode('utf-8').strip().split('<div')[0]
+        plot = py2_encode(client.parseDOM(info, 'div', attrs={'class': 'leiras'})[0]).strip().split('<div')[0]
         time = client.parseDOM(info, 'div', attrs={'class': 'infotab-time'})[0]
         duration = int(time.replace(" Perc", "").strip())*60
         center = client.parseDOM(url_content, 'div', attrs={'class': 'center'})[0]
@@ -126,18 +134,18 @@ class navigator:
         for season in seasons.splitlines():
             matches = re.search(r'^<a(.*)class="linktab"(.*)href="(.*)">(.*)</a>$', season.strip())
             if matches:
-                self.addDirectoryItem(u'%s. évad' % matches.group(4), 'episodes&url=%s&thumb=%s' % (urllib.quote_plus(matches.group(3)), urllib.quote_plus(thumb)), "%s%s" % (base_url, thumb), 'DefaultMovies.png', meta={'title': title, 'plot': plot, 'duration': duration}, banner=banner)
+                self.addDirectoryItem(u'%s. évad' % matches.group(4), 'episodes&url=%s&thumb=%s' % (quote_plus(matches.group(3)), quote_plus(thumb)), "%s%s" % (base_url, thumb), 'DefaultMovies.png', meta={'title': title, 'plot': plot, 'duration': duration}, banner=banner)
         self.endDirectory('tvshows')
 
     def getEpisodes(self, url, thumb):
         url_content = client.request('%s%s' %(base_url, url))
         title = client.parseDOM(url_content, 'div', attrs={'class': 'cim'})[0]
-        title = client.replaceHTMLCodes(client.parseDOM(title, 'h1')[0]).encode('utf-8').strip()
+        title = py2_encode(client.replaceHTMLCodes(client.parseDOM(title, 'h1')[0])).strip()
         panel = client.parseDOM(url_content, 'div', attrs={'class': 'panel'})[0]
         poster = client.parseDOM(panel, 'div', attrs={'class': 'poster'})[0]
         banner = '%s/%s' % (poster, client.parseDOM(panel, 'img', ret='src')[0])
         info = client.parseDOM(panel, 'div', attrs={'class': 'info'})[0]
-        plot = client.parseDOM(info, 'div', attrs={'class': 'leiras'})[0].encode('utf-8').strip().split('<div')[0]
+        plot = py2_encode(client.parseDOM(info, 'div', attrs={'class': 'leiras'})[0]).strip().split('<div')[0]
         time = client.parseDOM(info, 'div', attrs={'class': 'infotab-time'})[0]
         duration = int(time.replace(" Perc", "").strip())*60
         center = client.parseDOM(url_content, 'div', attrs={'class': 'center'})[0]
@@ -145,14 +153,14 @@ class navigator:
         for episode in episodes.splitlines():
             matches = re.search(r'(.*)<a(.*)class="linktab([^"]*)"(.*)href="(.*)">(.*)</a>$', episode.strip())
             if matches:
-                self.addDirectoryItem(u'%s. rész %s' % (matches.group(6), "| [COLOR limegreen]Feliratos[/COLOR]" if matches.group(3) == "_feliratos" else ""), 'movie&url=%s&thumb=%s&banner=%s&plot=%s' % (urllib.quote_plus(matches.group(5)), urllib.quote_plus(thumb), urllib.quote_plus(banner), urllib.quote_plus(plot)), "%s%s" % (base_url, thumb), 'DefaultMovies.png', isFolder=True, meta={'title': title, 'plot': plot, 'duration': duration}, banner=banner)
+                self.addDirectoryItem(u'%s. rész %s' % (matches.group(6), "| [COLOR limegreen]Feliratos[/COLOR]" if matches.group(3) == "_feliratos" else ""), 'movie&url=%s&thumb=%s&banner=%s&plot=%s' % (quote_plus(matches.group(5)), quote_plus(thumb), quote_plus(banner), quote_plus(plot)), "%s%s" % (base_url, thumb), 'DefaultMovies.png', isFolder=True, meta={'title': title, 'plot': plot, 'duration': duration}, banner=banner)
         self.endDirectory('episodes')
 
     def getMovie(self, url, thumb):
         url_content = client.request('%s%s' %(base_url, url))
         title = client.parseDOM(url_content, 'div', attrs={'class': 'cim'})[0]
-        title = client.replaceHTMLCodes(client.parseDOM(title, 'h1')[0]).encode('utf-8').strip()
-        plot = client.parseDOM(url_content, 'div', attrs={'class': 'leiras'})[0].encode('utf-8').strip().split('<div')[0]
+        title = py2_encode(client.replaceHTMLCodes(client.parseDOM(title, 'h1')[0])).strip()
+        plot = py2_encode(client.parseDOM(url_content, 'div', attrs={'class': 'leiras'})[0]).strip().split('<div')[0]
         time = client.parseDOM(url_content, 'div', attrs={'class': 'infotab-time'})[0]
         duration = int(time.replace(" Perc", "").strip())*60
         year = client.parseDOM(url_content, 'div', attrs={'class': 'infotab-time'})
@@ -167,7 +175,7 @@ class navigator:
             matches = re.search(r'^<a(.*)href="(.*)">(.*)</a>$', source.strip())
             if matches:
                 sourceCnt+=1
-                self.addDirectoryItem('%s | [B]%s[/B]' % (format(sourceCnt, '02'), matches.group(3)), 'playmovie&url=%s%s' % (url, urllib.quote_plus(matches.group(2))), "%s%s" % (base_url, thumb), 'DefaultMovies.png', isFolder=False, meta={'title': title, 'plot': plot, 'duration': duration}, banner="")
+                self.addDirectoryItem('%s | [B]%s[/B]' % (format(sourceCnt, '02'), matches.group(3)), 'playmovie&url=%s%s' % (url, quote_plus(matches.group(2))), "%s%s" % (base_url, thumb), 'DefaultMovies.png', isFolder=False, meta={'title': title, 'plot': plot, 'duration': duration}, banner="")
         self.endDirectory('movies')
 
     def playmovie(self, url):
@@ -178,16 +186,16 @@ class navigator:
         else:
             filmbeagyazas = client.parseDOM(url_content, 'div', attrs={'class': 'beagyazas'})[0]
         source = client.parseDOM(filmbeagyazas, 'iframe', ret='src')[0]
-        xbmc.log('Dmdamedia: resolving url: %s' % source, xbmc.LOGNOTICE)
+        xbmc.log('Dmdamedia: resolving url: %s' % source, xbmc.LOGINFO)
         try:
             direct_url = urlresolver.resolve(source)
             if direct_url:
-                direct_url = direct_url.encode('utf-8')
+                direct_url = py2_encode(direct_url)
         except Exception as e:
-            xbmcgui.Dialog().notification(urlparse.urlparse(url).hostname, e.message)
+            xbmcgui.Dialog().notification(urlparse.urlparse(url).hostname, str(e))
             return
         if direct_url:
-            xbmc.log('Dmdamedia: playing URL: %s' % direct_url, xbmc.LOGNOTICE)
+            xbmc.log('Dmdamedia: playing URL: %s' % direct_url, xbmc.LOGINFO)
             play_item = xbmcgui.ListItem(path=direct_url)
             xbmcplugin.setResolvedUrl(syshandle, True, listitem=play_item)
 
@@ -196,7 +204,7 @@ class navigator:
         if thumb == '': thumb = icon
         cm = []
         if queue == True: cm.append((queueMenu, 'RunPlugin(%s?action=queueItem)' % sysaddon))
-        if not context == None: cm.append((context[0].encode('utf-8'), 'RunPlugin(%s?action=%s)' % (sysaddon, context[1])))
+        if not context == None: cm.append((py2_encode(context[0]), 'RunPlugin(%s?action=%s)' % (sysaddon, context[1])))
         item = xbmcgui.ListItem(label=name)
         item.addContextMenuItems(cm)
         item.setArt({'icon': thumb, 'thumb': thumb, 'poster': thumb, 'banner': banner})
